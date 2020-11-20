b883c18df3cef45468ea79ce0b9c8a7edd463d20
liqiming-whu <liqiming@whu.edu.cn>
2020-11-20 20:12:58 +0800

    update

--- famine/articles/admin.py
+++ famine/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Article, Author, Author_Article, Journal
+
+
+admin.site.register(Article)
+admin.site.register(Author)
+admin.site.register(Author_Article)
+admin.site.register(Journal)
--- famine/famine/settings.py
+++ famine/famine/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'famine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'faminedb',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
